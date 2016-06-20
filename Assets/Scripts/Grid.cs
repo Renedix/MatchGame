ff11ca5e824d16d93ba323dd25564168dc122709
Damien Stevens <damiensebay88@googlemail.com>
2016-06-20 03:29:35 -0400

    Initial 01-02

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,44 +4,47 @@ using System.Collections.Generic;
 
 public class Grid : MonoBehaviour {
 
-    Dictionary<PieceType, GameObject> piecePrefabDictionary;
+	public enum PieceType
+	{
+		NORMAL,
+		COUNT,
+	};
 
-    public enum PieceType
-    {
-        NORMAL,
-        COUNT,
-    };
+	[System.Serializable]
+	public struct PiecePrefab
+	{
+		public PieceType type;
+		public GameObject prefab;
+	};
 
-    [System.Serializable]
-    public struct PiecePrefab {
-        public GameObject gameObject;
-        public PieceType pieceType;
-    };
+	public int xDim;
+	public int yDim;
 
-    public int xDim;
-    public int yDim;
-    public PiecePrefab[] prefabArray;
-    public GameObject backgroundPrefab;
-    
+	public PiecePrefab[] piecePrefabs;
+	public GameObject backgroundPrefab;
 
+	private Dictionary<PieceType, GameObject> piecePrefabDict;
+
+	// Use this for initialization
 	void Start () {
-        piecePrefabDictionary = new Dictionary<PieceType, GameObject>();
+		piecePrefabDict = new Dictionary<PieceType, GameObject> ();
 
-        foreach (PiecePrefab piece in prefabArray)
-        {
-            if (!piecePrefabDictionary.ContainsKey(piece.pieceType))
-            {
-                piecePrefabDictionary.Add(piece.pieceType, piece.gameObject);
-            }
-        }
+		for (int i = 0; i < piecePrefabs.Length; i++) {
+			if (!piecePrefabDict.ContainsKey (piecePrefabs [i].type)) {
+				piecePrefabDict.Add (piecePrefabs [i].type, piecePrefabs [i].prefab);
+			}
+		}
 
-        for(int x = 0; x < xDim; x++)
-        {
-            for(int y = 0; y < yDim; y++)
-            {
-                GameObject background = (GameObject) Instantiate(backgroundPrefab,new Vector3(x,y,0),Quaternion.identity);
-                background.transform.parent = transform;
-            }
-        }
+		for (int x = 0; x < xDim; x++) {
+			for (int y = 0; y < yDim; y++) {
+				GameObject background = (GameObject)Instantiate (backgroundPrefab, new Vector3 (x, y, 0), Quaternion.identity);
+				background.transform.parent = transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
 	}
 }
